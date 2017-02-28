77477cfcec8a02759ed6b7996fb350a9ff6b3d35
luccaluke <loeloy14@gmail.com>
2017-02-28 00:01:18 -0500

    Add files via upload

--- spell-checker.lua
+++ spell-checker.lua
@@ -6,12 +6,43 @@ Date: 2/29/2017
 Checks the spelling of a .txt file and returns a list of the misspelled words.
 --]]
 
-local dictionaryFile = "C:\\Users\\Tyler\\Documents\\Lua\\Programs\\common-program\\dictionary.txt"
+--local dictionaryFile = "C:\\Users\\Tyler\\Documents\\Lua\\Programs\\common-program\\dictionary.txt"
+local dictionaryFile = "dictionary.txt"
+
 
 local function printInstructions()
   -- Prints the instructions
   print("Please input 1 to enter a text file to spell check, input 2 to enter a new word into the dictionary, or input 3 to exit the program: ")
 end
+
+local function spellCheck(path)
+  -- Runs through and spell checks .txt file
+  local txt = io.open(path, "r")
+  io.input(txt)
+  local txtStr =  txt:read("*all")
+  txt:close()
+  local words = {}
+  txtStr:gsub("%s",function(c) table.insert(words,c) end)
+  print (#words)
+  print(words[2])
+  if check(words[1]) then
+    print("yes")
+  else
+    print("no")
+  end
+  
+  
+      
+    
+  end 
+function check(word)
+  for i = 1, #a do
+    if word == a[i] then
+      return true
+      end
+  end
+  return false
+  end
 
 -- Download the dictionary into a table
 dic = io.open(dictionaryFile, "r")
@@ -20,7 +51,7 @@ a = {}
 for line in io.lines() do
   table.insert(a, line)
 end
-print(table.getn(a))
+print(#a)
 dic:close()
 
 -- Print instructions
@@ -29,15 +60,16 @@ print("Welcome to the spell-checker.")
 -- Read input
 ::getChoice::
 printInstructions()
-input = io.stdin:read("*n")
+input = io.stdin:read("*l")
 
 -- Process selection
-if input == 1 then
+if input == "1" then
   print("Please input the path name to a .txt file: ")
-  path = io.stdin:read("*all")
+  repeat path = io.stdin:read() until path ~= "\n"
+  print(path)
   spellCheck(path)
   goto getChoice
-elseif input == 2 then
+elseif input == "2" then
   print("Please input a word to add to the dictionary: ")
   word = io.stdin:read("*l")
   f = io.open(dictionaryFile, "a")
@@ -46,7 +78,7 @@ elseif input == 2 then
   io.close(f)
   print("Successfully added word to the dictionary!")
   goto getChoice  
-elseif input == 3 then
+elseif input == "3" then
   goto exit
 else
   print("Incorrect input.")
